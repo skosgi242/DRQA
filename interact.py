a2df20eee5303f8bb0f30884d052c22898b11664
skosgi242 <51510948+skosgi242@users.noreply.github.com>
2020-11-30 00:05:15 +0530

    Update interact.py

--- interact.py
+++ interact.py
@@ -6,18 +6,12 @@ from samples import __generate_samples
 
 
 
-question = "Which NFL team represented the AFC at Super Bowl 50?"
-context = "Super Bowl 50 was an American football game to determine the champion of the National Football League (NFL) for the 2015 season. The American Football Conference (AFC) champion Denver Broncos defeated the National Football Conference (NFC) champion Carolina Panthers 24–10 to earn their third Super Bowl title. The game was played on February 7, 2016, at Levi's Stadium in the San Francisco Bay Area at Santa Clara, California. As this was the 50th Super Bowl, the league emphasized the \"golden anniversary\" with various gold-themed initiatives, as well as temporarily suspending the tradition of naming each Super Bowl game with Roman numerals (under which the game would have been known as \"Super Bowl L\"), so that the logo could prominently feature the Arabic numerals 50."
-
-pathfile = "/Users/skosgi/Downloads/network26"
-
-
 def __get_vocab_set(path):
     with open(path,"rb") as f:
         return pickle.load(f)
 
-def predict(question,context,net_path,model):
-    vocab_set = __get_vocab_set("vocab_set")
+def predict(question,context,net_path,model,vocabfile):
+    vocab_set = __get_vocab_set(vocabfile)
     embedding = torch.tensor(vocab_set['embeddings'])
     embedding = embedding.float()
 
@@ -41,5 +35,5 @@ def predict(question,context,net_path,model):
     samples, _ = __generate_batches(samples, 2, config['tag_size'], config['ent_size'], 'eval')
 
     tr = Train(config, embedding, model)
-    print(tr.loadandpredict(samples, True, pathfile))
+    return tr.loadandpredict(samples, True, net_path)
 
