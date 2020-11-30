2a3926058650be1e417834518e93b7c4a9b4ab0a
skosgi242 <51510948+skosgi242@users.noreply.github.com>
2020-11-30 22:02:22 +0530

    Update and rename testinteract.py to Document Reader/testinteract.py

---
+++ Document Reader/testinteract.py
@@ -0,0 +1,55 @@
+from interact import predict
+import string
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+wikifile = "wiki_10000.pkl"
+invfile = "inverted_index_10000.pkl"
+pathfile = "/Users/skosgi/Downloads/drqanetwork"
+question = "Where does india stand in troop contributor united nations?"
+vocab_path =  "vocab_set"
+
+
+wiki = pickle.load(open(wikifile,'rb'))
+inverted_index = pickle.load(open(invfile,'rb'))
+
+
+class DocumentRetriever:
+    def __init__(self):
+        return
+    # pass question as q
+    def find(self, q='What is anarchism?', count=5):
+        # split into words
+        q = q.lower().translate(str.maketrans('', '', string.punctuation)).split()
+
+        # inverted-index lookup
+        titles = []
+        for q_word in q:
+            titles.extend(inverted_index[q_word])
+        titles = list(set(titles))
+
+        # prepare corpus of articles found in inverted-index lookup
+        question = ' '.join(q)
+        corpus = [question]
+        for (index, title) in titles:
+            text = wiki.iloc[index, 0].upper() + ' | ' + wiki.iloc[index, 1]
+            corpus.append(str(text))
+
+        # TF-IDF on articles found in inverted-index lookup
+        tfidf = TfidfVectorizer()
+        vecs = tfidf.fit_transform(corpus)
+        corr_matrix = ((vecs * vecs.T).A)
+        result = [corpus[i] for i in np.argsort(corr_matrix[0])[::-1][:count+1]]
+
+        return result[1:]
+
+
+ret = DocumentRetriever()
+
+contexts = ret.find(q=question, count=2)
+con = ""
+
+
+for context in contexts:
+    print(predict(question,context,pathfile,"RNN",vocab_path))
